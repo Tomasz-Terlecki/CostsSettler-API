6c981380b3d33c5b4bc9915cdfab2283b1174a1c
Tomasz Terlecki <t.terlecki@wp.pl>
2023-06-17 12:51:14 +0200

    API config update - add necessary components to Program.cs and launchSettings

--- src/CostsSettler.API/Program.cs
+++ src/CostsSettler.API/Program.cs
@@ -1,6 +1,54 @@
+using CostsSettler.Domain.Queries;
+using CostsSettler.Repo;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+var env = builder.Environment;
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetCircumstancesQuery).Assembly));
+
+builder.Services.AddDbContext<CostsSettlerDbContext>(
+    options => options.UseSqlServer(config.GetConnectionString("DefultConnection"),
+    builder =>
+    {
+        builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+        builder.MaxBatchSize(1);
+    }));
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(o =>
+    o.AddPolicy("AllowAll", builder =>
+    {
+        builder
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .SetIsOriginAllowed(origin => true)
+            .AllowCredentials();
+    }));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (env.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
