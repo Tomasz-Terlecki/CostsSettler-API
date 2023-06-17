04fb354dc84de46c64227e92e47ec970b72e8637
Tomasz Terlecki <t.terlecki@wp.pl>
2023-06-17 12:48:41 +0200

    Move handler to nested class

--- src/CostsSettler.Domain/Queries/GetCircumstancesQuery.cs
+++ src/CostsSettler.Domain/Queries/GetCircumstancesQuery.cs
@@ -4,4 +4,19 @@ using MediatR;
 namespace CostsSettler.Domain.Queries;
 public class GetCircumstancesQuery : IRequest<ICollection<CircumstanceForListDto>>
 {
+    public string Description { get; set; }
+
+    public class GetCircumstancesQueryHandler : IRequestHandler<GetCircumstancesQuery, ICollection<CircumstanceForListDto>>
+    {
+        public async Task<ICollection<CircumstanceForListDto>> Handle(GetCircumstancesQuery request, CancellationToken cancellationToken)
+        {
+            return new List<CircumstanceForListDto>
+            {
+                new CircumstanceForListDto
+                {
+                    Description = request.Description
+                }
+            };
+        }
+    }
 }
