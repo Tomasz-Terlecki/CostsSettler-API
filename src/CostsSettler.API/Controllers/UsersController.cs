e77b6f305c1b03555b1dee8fc3672ca4ce13d927
Tomasz Terlecki <t.terlecki@wp.pl>
2023-06-26 15:59:46 +0200

    Getting users from Keycloak - controller and query implementation

---
+++ src/CostsSettler.API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using CostsSettler.Domain.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CostsSettler.API.Controllers;
+
+[ApiController]
+[Route("/api/[Controller]")]
+public class UsersController
+{
+    private readonly IMediator _mediator;
+
+    public UsersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var result = await _mediator.Send(new GetUsersQuery());
+
+        if (result == null)
+            return new BadRequestObjectResult("Getting users failed");
+
+        return new OkObjectResult(result);
+    }
+}
--- src/CostsSettler.Domain/Queries/GetUsersQuery.cs
+++ src/CostsSettler.Domain/Queries/GetUsersQuery.cs
@@ -1,8 +1,26 @@
-﻿using CostsSettler.Domain.Models;
+﻿using AutoMapper;
+using CostsSettler.Domain.Dtos;
+using CostsSettler.Domain.Interfaces.Repositories;
 using MediatR;
 
 namespace CostsSettler.Domain.Queries;
-public class GetUsersQuery : IRequest<ICollection<User>>
+public class GetUsersQuery : IRequest<ICollection<UserForListDto>>
 {
-    
+    public class GetUsersQueryHandler : IRequestHandler<GetUsersQuery, ICollection<UserForListDto>>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public GetUsersQueryHandler(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ICollection<UserForListDto>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+        {
+            var users = await _userRepository.GetAllAsync();
+            return _mapper.Map<ICollection<UserForListDto>>(users);
+        }
+    }
 }
