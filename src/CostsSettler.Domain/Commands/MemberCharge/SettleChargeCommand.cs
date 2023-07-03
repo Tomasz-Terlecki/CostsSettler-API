3307a47e848c8f217b71a248fb4d18ca86ea21f2
Tomasz Terlecki <t.terlecki@wp.pl>
2023-07-03 20:27:05 +0200

    SettleChargeCommand implementation

--- src/CostsSettler.Domain/Commands/MemberCharge/SettleChargeCommand.cs
+++ src/CostsSettler.Domain/Commands/MemberCharge/SettleChargeCommand.cs
@@ -1,4 +1,6 @@
-﻿using CostsSettler.Domain.Interfaces.Repositories;
+﻿using CostsSettler.Domain.Enums;
+using CostsSettler.Domain.Interfaces.Repositories;
+using CostsSettler.Domain.Models;
 using MediatR;
 
 namespace CostsSettler.Domain.Commands;
@@ -9,22 +11,34 @@ public class SettleChargeCommand : IRequest<bool>
     public class SettleChargeCommandHandler : IRequestHandler<SettleChargeCommand, bool>
     {
         private readonly IMemberChargeRepository _repository;
+        private readonly ICircumstanceRepository _circumstanceRepository;
 
-        public SettleChargeCommandHandler(IMemberChargeRepository repository)
+        public SettleChargeCommandHandler(IMemberChargeRepository repository, ICircumstanceRepository circumstanceRepository)
         {
             _repository = repository;
+            _circumstanceRepository = circumstanceRepository;
         }
 
         public async Task<bool> Handle(SettleChargeCommand request, CancellationToken cancellationToken)
         {
             var charge = await _repository.GetByIdAsync(request.MemberChargeId);
 
-            if (charge is null)
+            if (charge is null || charge.CircumstanceRole != CircumstanceRole.Creditor)
                 return false;
 
-            await _repository.UpdateAsync(charge);
+            charge.ChargeStatus = ChargeStatus.Settled;
 
-            return true;
+            var circumstance = await _circumstanceRepository.GetByIdAsync(charge.CircumstanceId, new string[] { nameof(Circumstance.Members) });
+
+            if (circumstance is not null &&
+                circumstance.Members is not null &&
+                circumstance.Members.All(charge => charge.ChargeStatus == ChargeStatus.Settled))
+            {
+                circumstance.CircumstanceStatus = CircumstanceStatus.Settled;
+                await _circumstanceRepository.UpdateAsync(circumstance);
+            }
+
+            return await _repository.UpdateAsync(charge);
         }
     }
 }
