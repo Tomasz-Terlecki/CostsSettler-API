992a9e459b7bc636c2da074083d82d99c3e2937d
Tomasz Terlecki <t.terlecki@wp.pl>
2023-07-03 20:26:55 +0200

    IUserRepository - remove deriving from IRepositoryBase

--- src/CostsSettler.Domain/Interfaces/Repositories/IUserRepository.cs
+++ src/CostsSettler.Domain/Interfaces/Repositories/IUserRepository.cs
@@ -1,6 +1,12 @@
 ﻿using CostsSettler.Domain.Models;
 
 namespace CostsSettler.Domain.Interfaces.Repositories;
-public interface IUserRepository : IRepositoryBase<User>
+public interface IUserRepository
 {
+    Task<ICollection<User>> GetAllAsync();
+    Task<User?> GetByIdAsync(Guid id);
+    Task<bool> AddAsync(User model);
+    Task<bool> UpdateAsync(User model);
+    Task<bool> DeleteAsync(Guid id);
+    Task<bool> ExistsAsync(Guid id);
 }
