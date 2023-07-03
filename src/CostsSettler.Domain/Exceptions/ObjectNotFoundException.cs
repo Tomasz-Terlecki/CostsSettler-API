dd629510624fe1370891378187ed3c9d26bbc7b8
Tomasz Terlecki <t.terlecki@wp.pl>
2023-07-03 21:14:41 +0200

    Add ObjectNotFoundException and use in GetCircumstance

---
+++ src/CostsSettler.Domain/Exceptions/ObjectNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace CostsSettler.Domain.Exceptions;
+public class ObjectNotFoundException : DomainLogicException
+{
+    public ObjectNotFoundException(Type type, Guid id) : base($"Could not find object of type {nameof(type)} with Id {id}")
+    {
+    }
+}
--- src/CostsSettler.Domain/Queries/Circumstance/GetCircumstanceByIdQuery.cs
+++ src/CostsSettler.Domain/Queries/Circumstance/GetCircumstanceByIdQuery.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using CostsSettler.Domain.Dtos;
+using CostsSettler.Domain.Exceptions;
 using CostsSettler.Domain.Interfaces.Repositories;
 using CostsSettler.Domain.Models;
 using MediatR;
@@ -13,16 +14,25 @@ public class GetCircumstanceByIdQuery : IRequest<CircumstanceForReturnDto>
     {
         private readonly ICircumstanceRepository _repository;
         private readonly IMapper _mapper;
+        private readonly IUserRepository _userRepository;
 
-        public GetCircumstanceByIdQueryHandler(ICircumstanceRepository repository, IMapper mapper)
+        public GetCircumstanceByIdQueryHandler(ICircumstanceRepository repository, IMapper mapper, IUserRepository userRepository)
         {
             _repository = repository;
             _mapper = mapper;
+            _userRepository = userRepository;
         }
 
         public async Task<CircumstanceForReturnDto> Handle(GetCircumstanceByIdQuery request, CancellationToken cancellationToken)
         {
             var circumstance = await _repository.GetByIdAsync(request.Id, new string[] { nameof(Circumstance.Members) });
+
+            if (circumstance == null)
+                throw new ObjectNotFoundException(typeof(Circumstance), request.Id);
+
+            foreach (var member in circumstance.Members ?? new List<MemberCharge>())
+                member.User = await _userRepository.GetByIdAsync(member.UserId) 
+                    ?? throw new ObjectNotFoundException(typeof(User), member.UserId);
 
             return _mapper.Map<CircumstanceForReturnDto>(circumstance);
         }
