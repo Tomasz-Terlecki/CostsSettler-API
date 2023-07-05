6202a636ae0e8f79d8bcf548393630a49d68ed32
Tomasz Terlecki <t.terlecki@wp.pl>
2023-07-05 15:44:50 +0200

    New mgirations generated

---
+++ src/CostsSettler.Repo/Migrations/20230705132851_Update.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CostsSettler.Repo.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CircumstanceStatus",
+                table: "Circumstances");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "CircumstanceStatus",
+                table: "Circumstances",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+    }
+}
