cf6c3557bacdc587a5c302caca12dabc2ae8d599
Tomasz Terlecki <t.terlecki@wp.pl>
2023-07-25 15:50:54 +0200

    Remove redundant properties from circumstance for list dto

--- src/CostsSettler.Domain/Dtos/Circumstance/CircumstanceForListDto.cs
+++ src/CostsSettler.Domain/Dtos/Circumstance/CircumstanceForListDto.cs
@@ -6,9 +6,5 @@ public class CircumstanceForListDto
     public Guid Id { get; set; }
     public string Description { get; set; } = null!;
     public CircumstanceStatus CircumstanceStatus { get; set; }
-    public Guid CreditorId { get; set; }
-    public UserForListDto Creditor { get; set; } = null!;
-    public Guid DebtorId { get; set; }
-    public UserForListDto Debtor { get; set; } = null!;
     public DateTime DateTime { get; set; }
 }
