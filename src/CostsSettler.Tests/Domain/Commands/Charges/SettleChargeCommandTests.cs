e00193e374d4e5e5e6d83ae1419d61158bda7ec2
Tomasz Terlecki <t.terlecki@wp.pl>
2023-08-25 16:34:37 +0200

    SettleCharge and VoteForCharge tests

---
+++ src/CostsSettler.Tests/Domain/Commands/Charges/SettleChargeCommandTests.cs
@@ -0,0 +1,230 @@
+﻿using CostsSettler.Domain.Commands;
+using CostsSettler.Domain.Enums;
+using CostsSettler.Domain.Exceptions;
+using CostsSettler.Domain.Interfaces.Repositories;
+using CostsSettler.Domain.Models;
+using CostsSettler.Domain.Queries;
+using CostsSettler.Domain.Services;
+using CostsSettler.Tests.Helpers;
+using MediatR;
+using Moq;
+
+namespace CostsSettler.Tests.Domain.Commands.Charges;
+public class SettleChargeCommandTests
+{
+    private Mock<IChargeRepository> _chargeRepositoryMock { get; }
+    private Mock<ICircumstanceRepository> _circumstanceRepositoryMock { get; }
+    private Mock<IUserRepository> _userRepositoryMock { get; }
+    private Mock<IIdentityService> _identityServiceMock { get; }
+    private Mock<IMediator> _mediatorMock { get; }
+
+    private RandomChargeFactory _randomChargeFactory { get; }
+    private RandomCircumstanceFactory _randomCircumstanceFactory { get; }
+    private RandomUserFactory _randomUserFactory { get; }
+
+    public SettleChargeCommandTests()
+    {
+        _chargeRepositoryMock = new Mock<IChargeRepository>();
+        _circumstanceRepositoryMock = new Mock<ICircumstanceRepository>();
+        _userRepositoryMock = new Mock<IUserRepository>();
+        _identityServiceMock = new Mock<IIdentityService>();
+        _mediatorMock = new Mock<IMediator>();
+
+        _randomChargeFactory = new RandomChargeFactory();
+        _randomCircumstanceFactory = new RandomCircumstanceFactory();
+        _randomUserFactory = new RandomUserFactory();
+    }
+
+    [Theory]
+    [InlineData(ChargeStatus.Accepted, CircumstanceStatus.Accepted)]
+    [InlineData(ChargeStatus.Accepted, CircumstanceStatus.PartiallySettled)]
+    public void SettleCharge_SettleValidChargeStatusAndCircumstanceStatus_Test(ChargeStatus chargeStatus, CircumstanceStatus circumstanceStatus)
+    {
+        var chargeId = Guid.NewGuid();
+        var circumstanceId = Guid.NewGuid();
+        var circumstance = _randomCircumstanceFactory.Create(circumstanceId, new CircumstanceAttributes { CircumstanceStatus = circumstanceStatus });
+        var charge = _randomChargeFactory.Create(chargeId, new ChargeAttributes
+        {
+            CircumstanceId = circumstanceId,
+            Circumstance = circumstance,
+            Creditor = _randomUserFactory.Create(),
+            ChargeStatus = chargeStatus
+        });
+        circumstance.Charges = new List<Charge> { charge };
+        
+        _mediatorMock
+            .Setup(mediator => mediator.Send(It.IsAny<GetChargeByIdQuery>(), CancellationToken.None))
+            .ReturnsAsync(charge);
+
+        _mediatorMock
+            .Setup(mediator => mediator.Send(It.IsAny<GetCircumstanceByIdQuery>(), CancellationToken.None))
+            .ReturnsAsync(circumstance);
+
+        _identityServiceMock
+            .Setup(service => service.CheckEqualityWithLoggedUserId(charge.CreditorId))
+            .Verifiable();
+
+        _circumstanceRepositoryMock
+            .Setup(repo => repo.UpdateAsync(It.IsAny<Circumstance>()))
+            .ReturnsAsync(true);
+
+        _chargeRepositoryMock
+            .Setup(repo => repo.UpdateAsync(It.IsAny<Charge>()))
+            .ReturnsAsync(true);
+
+        var command = new SettleChargeCommand { ChargeId = chargeId };
+
+        var commandHandler = new SettleChargeCommand.SettleChargeCommandHandler(
+            _mediatorMock.Object,
+            _chargeRepositoryMock.Object,
+            _identityServiceMock.Object,
+            _circumstanceRepositoryMock.Object
+        );
+
+        var result = commandHandler.Handle(command, CancellationToken.None).Result;
+
+        _circumstanceRepositoryMock.Verify(repo =>
+            repo.UpdateAsync(It.Is<Circumstance>(circumstance => 
+                circumstance.CircumstanceStatus == CircumstanceStatus.Settled ||
+                circumstance.CircumstanceStatus == CircumstanceStatus.PartiallySettled)),
+            Times.Once);
+
+        _chargeRepositoryMock.Verify(repo =>
+            repo.UpdateAsync(It.Is<Charge>(charge =>
+                charge.ChargeStatus == ChargeStatus.Settled)),
+            Times.Once);
+
+        _identityServiceMock.Verify(service =>
+            service.CheckEqualityWithLoggedUserId(It.IsAny<Guid>()),
+            Times.Once);
+
+        Assert.True(result);
+    }
+
+    [Theory]
+    [InlineData(ChargeStatus.None, CircumstanceStatus.Accepted)]
+    [InlineData(ChargeStatus.New, CircumstanceStatus.Accepted)]
+    [InlineData(ChargeStatus.Rejected, CircumstanceStatus.Accepted)]
+    [InlineData(ChargeStatus.Settled, CircumstanceStatus.Accepted)]
+    [InlineData(ChargeStatus.Accepted, CircumstanceStatus.None)]
+    [InlineData(ChargeStatus.Accepted, CircumstanceStatus.New)]
+    [InlineData(ChargeStatus.Accepted, CircumstanceStatus.PartiallyAccepted)]
+    [InlineData(ChargeStatus.Accepted, CircumstanceStatus.Settled)]
+    [InlineData(ChargeStatus.Accepted, CircumstanceStatus.Rejected)]
+    public void SettleCharge_SettleInvalidChargeOrCircumstanceStatus_Test(ChargeStatus chargeStatus, CircumstanceStatus circumstanceStatus)
+    {
+        var chargeId = Guid.NewGuid();
+        var circumstanceId = Guid.NewGuid();
+        var circumstance = _randomCircumstanceFactory.Create(circumstanceId, new CircumstanceAttributes { CircumstanceStatus = circumstanceStatus });
+        var charge = _randomChargeFactory.Create(chargeId, new ChargeAttributes
+        {
+            CircumstanceId = circumstanceId,
+            Circumstance = circumstance,
+            Creditor = _randomUserFactory.Create(),
+            ChargeStatus = chargeStatus
+        });
+        circumstance.Charges = new List<Charge> { charge };
+
+        _mediatorMock
+            .Setup(mediator => mediator.Send(It.IsAny<GetChargeByIdQuery>(), CancellationToken.None))
+            .ReturnsAsync(charge);
+
+        _mediatorMock
+            .Setup(mediator => mediator.Send(It.IsAny<GetCircumstanceByIdQuery>(), CancellationToken.None))
+            .ReturnsAsync(circumstance);
+
+        _identityServiceMock
+            .Setup(service => service.CheckEqualityWithLoggedUserId(charge.CreditorId))
+            .Verifiable();
+
+        _circumstanceRepositoryMock
+            .Setup(repo => repo.UpdateAsync(It.IsAny<Circumstance>()))
+            .ReturnsAsync(true);
+
+        _chargeRepositoryMock
+            .Setup(repo => repo.UpdateAsync(It.IsAny<Charge>()))
+            .ReturnsAsync(true);
+
+        var command = new SettleChargeCommand { ChargeId = chargeId };
+
+        var commandHandler = new SettleChargeCommand.SettleChargeCommandHandler(
+            _mediatorMock.Object,
+            _chargeRepositoryMock.Object,
+            _identityServiceMock.Object,
+            _circumstanceRepositoryMock.Object
+        );
+
+        _circumstanceRepositoryMock.Verify(repo =>
+            repo.UpdateAsync(It.Is<Circumstance>(circumstance =>
+                circumstance.CircumstanceStatus == CircumstanceStatus.Settled ||
+                circumstance.CircumstanceStatus == CircumstanceStatus.PartiallySettled)),
+            Times.Never);
+
+        _chargeRepositoryMock.Verify(repo =>
+            repo.UpdateAsync(It.Is<Charge>(charge =>
+                charge.ChargeStatus == ChargeStatus.Settled)),
+            Times.Never);
+
+        Assert.ThrowsAsync<DomainLogicException>(
+            () => commandHandler.Handle(command, CancellationToken.None));
+    }
+
+    [Fact]
+    public void SettleCharge_UnauthorizedUser_Test()
+    {
+        var chargeId = Guid.NewGuid();
+        var circumstanceId = Guid.NewGuid();
+        var circumstance = _randomCircumstanceFactory.Create(circumstanceId, new CircumstanceAttributes { CircumstanceStatus = CircumstanceStatus.Accepted });
+        var charge = _randomChargeFactory.Create(chargeId, new ChargeAttributes
+        {
+            CircumstanceId = circumstanceId,
+            Circumstance = circumstance,
+            Creditor = _randomUserFactory.Create(),
+            ChargeStatus = ChargeStatus.Accepted
+        });
+        circumstance.Charges = new List<Charge> { charge };
+
+        _mediatorMock
+            .Setup(mediator => mediator.Send(It.IsAny<GetChargeByIdQuery>(), CancellationToken.None))
+            .ReturnsAsync(charge);
+
+        _mediatorMock
+            .Setup(mediator => mediator.Send(It.IsAny<GetCircumstanceByIdQuery>(), CancellationToken.None))
+            .ReturnsAsync(circumstance);
+
+        _identityServiceMock
+            .Setup(service => service.CheckEqualityWithLoggedUserId(charge.CreditorId))
+            .Throws<AuthorizationException>();
+
+        _circumstanceRepositoryMock
+            .Setup(repo => repo.UpdateAsync(It.IsAny<Circumstance>()))
+            .ReturnsAsync(true);
+
+        _chargeRepositoryMock
+            .Setup(repo => repo.UpdateAsync(It.IsAny<Charge>()))
+            .ReturnsAsync(true);
+
+        var command = new SettleChargeCommand { ChargeId = chargeId };
+
+        var commandHandler = new SettleChargeCommand.SettleChargeCommandHandler(
+            _mediatorMock.Object,
+            _chargeRepositoryMock.Object,
+            _identityServiceMock.Object,
+            _circumstanceRepositoryMock.Object
+        );
+
+        _circumstanceRepositoryMock.Verify(repo =>
+            repo.UpdateAsync(It.Is<Circumstance>(circumstance =>
+                circumstance.CircumstanceStatus == CircumstanceStatus.Settled ||
+                circumstance.CircumstanceStatus == CircumstanceStatus.PartiallySettled)),
+            Times.Never);
+
+        _chargeRepositoryMock.Verify(repo =>
+            repo.UpdateAsync(It.Is<Charge>(charge =>
+                charge.ChargeStatus == ChargeStatus.Settled)),
+            Times.Never);
+
+        Assert.ThrowsAsync<AuthorizationException>(
+            () => commandHandler.Handle(command, CancellationToken.None));
+    }
+}
---
+++ src/CostsSettler.Tests/Domain/Commands/Charges/VoteForChargeCommandTests.cs
@@ -0,0 +1,228 @@
+﻿using CostsSettler.Domain.Commands;
+using CostsSettler.Domain.Enums;
+using CostsSettler.Domain.Exceptions;
+using CostsSettler.Domain.Interfaces.Repositories;
+using CostsSettler.Domain.Models;
+using CostsSettler.Domain.Queries;
+using CostsSettler.Domain.Services;
+using CostsSettler.Tests.Helpers;
+using MediatR;
+using Moq;
+
+namespace CostsSettler.Tests.Domain.Commands.Charges;
+public class VoteForChargeCommandTests
+{
+    private Mock<IChargeRepository> _chargeRepositoryMock { get; }
+    private Mock<ICircumstanceRepository> _circumstanceRepositoryMock { get; }
+    private Mock<IUserRepository> _userRepositoryMock { get; }
+    private Mock<IIdentityService> _identityServiceMock { get; }
+    private Mock<IMediator> _mediatorMock { get; }
+
+    private RandomChargeFactory _randomChargeFactory { get; }
+    private RandomCircumstanceFactory _randomCircumstanceFactory { get; }
+    private RandomUserFactory _randomUserFactory { get; }
+
+    public VoteForChargeCommandTests()
+    {
+        _chargeRepositoryMock = new Mock<IChargeRepository>();
+        _circumstanceRepositoryMock = new Mock<ICircumstanceRepository>();
+        _userRepositoryMock = new Mock<IUserRepository>();
+        _identityServiceMock = new Mock<IIdentityService>();
+        _mediatorMock = new Mock<IMediator>();
+
+        _randomChargeFactory = new RandomChargeFactory();
+        _randomCircumstanceFactory = new RandomCircumstanceFactory();
+        _randomUserFactory = new RandomUserFactory();
+    }
+
+    [Theory]
+    [InlineData(ChargeStatus.None, CircumstanceStatus.None, ChargeVote.Accept)]
+    [InlineData(ChargeStatus.None, CircumstanceStatus.None, ChargeVote.Reject)]
+    [InlineData(ChargeStatus.New, CircumstanceStatus.New, ChargeVote.Accept)]
+    [InlineData(ChargeStatus.New, CircumstanceStatus.New, ChargeVote.Reject)]
+    [InlineData(ChargeStatus.Rejected, CircumstanceStatus.Rejected, ChargeVote.Accept)]
+    [InlineData(ChargeStatus.Rejected, CircumstanceStatus.Rejected, ChargeVote.Reject)]
+    [InlineData(ChargeStatus.Accepted, CircumstanceStatus.Accepted, ChargeVote.Accept)]
+    [InlineData(ChargeStatus.Accepted, CircumstanceStatus.Accepted, ChargeVote.Reject)]
+    [InlineData(ChargeStatus.Accepted, CircumstanceStatus.PartiallyAccepted, ChargeVote.Accept)]
+    [InlineData(ChargeStatus.Accepted, CircumstanceStatus.PartiallyAccepted, ChargeVote.Reject)]
+    public void VoteForCharge_ValidCircumstanceStatusAndChargeStatus_Test(
+        ChargeStatus chargeStatus, CircumstanceStatus circumstanceStatus, ChargeVote chargeVote)
+    {
+        var chargeId = Guid.NewGuid();
+        var circumstanceId = Guid.NewGuid();
+        var circumstance = _randomCircumstanceFactory.Create(circumstanceId, new CircumstanceAttributes { CircumstanceStatus = circumstanceStatus });
+        var charge = _randomChargeFactory.Create(chargeId, new ChargeAttributes
+        {
+            CircumstanceId = circumstanceId,
+            Circumstance = circumstance,
+            Debtor = _randomUserFactory.Create(),
+            ChargeStatus = chargeStatus
+        });
+        circumstance.Charges = new List<Charge> { charge };
+
+        _mediatorMock
+            .Setup(mediator => mediator.Send(It.IsAny<GetChargeByIdQuery>(), CancellationToken.None))
+            .ReturnsAsync(charge);
+
+        _mediatorMock
+            .Setup(mediator => mediator.Send(It.IsAny<GetCircumstanceByIdQuery>(), CancellationToken.None))
+            .ReturnsAsync(circumstance);
+
+        _identityServiceMock
+            .Setup(service => service.CheckEqualityWithLoggedUserId(charge.DebtorId))
+            .Verifiable();
+
+        _circumstanceRepositoryMock
+            .Setup(repo => repo.UpdateAsync(It.IsAny<Circumstance>()))
+            .ReturnsAsync(true);
+
+        _chargeRepositoryMock
+            .Setup(repo => repo.UpdateAsync(It.IsAny<Charge>()))
+            .ReturnsAsync(true);
+
+        var command = new VoteForChargeCommand { ChargeId = chargeId, ChargeVote = chargeVote };
+
+        var commandHandler = new VoteForChargeCommand.VoteForChargeCommandHandler(
+            _mediatorMock.Object,
+            _chargeRepositoryMock.Object,
+            _identityServiceMock.Object,
+            _circumstanceRepositoryMock.Object
+        );
+
+        var result = commandHandler.Handle(command, CancellationToken.None).Result;
+
+        _circumstanceRepositoryMock.Verify(repo =>
+            repo.UpdateAsync(It.Is<Circumstance>(circumstance =>
+                chargeVote == ChargeVote.Accept
+                    ? circumstance.CircumstanceStatus == CircumstanceStatus.Accepted ||
+                      circumstance.CircumstanceStatus == CircumstanceStatus.PartiallyAccepted
+                    : circumstance.CircumstanceStatus == CircumstanceStatus.Rejected)),
+             Times.Once);
+
+        _chargeRepositoryMock.Verify(repo =>
+            repo.UpdateAsync(It.Is<Charge>(charge =>
+                charge.ChargeStatus == (chargeVote == ChargeVote.Accept
+                    ? ChargeStatus.Accepted
+                    : ChargeStatus.Rejected))),
+            Times.Once);
+
+        _identityServiceMock.Verify(repo =>
+            repo.CheckEqualityWithLoggedUserId(It.IsAny<Guid>()),
+            Times.Once);
+
+        Assert.True(result);
+    }
+
+    [Theory]
+    [InlineData(ChargeStatus.Settled, CircumstanceStatus.PartiallyAccepted, ChargeVote.Reject)]
+    [InlineData(ChargeStatus.New, CircumstanceStatus.Settled, ChargeVote.Reject)]
+    [InlineData(ChargeStatus.New, CircumstanceStatus.PartiallySettled, ChargeVote.Reject)]
+    public void VoteForCharge_InValidCircumstanceStatusOrChargeStatus_Test(
+        ChargeStatus chargeStatus, CircumstanceStatus circumstanceStatus, ChargeVote chargeVote)
+    {
+        var chargeId = Guid.NewGuid();
+        var circumstanceId = Guid.NewGuid();
+        var circumstance = _randomCircumstanceFactory.Create(circumstanceId, new CircumstanceAttributes { CircumstanceStatus = circumstanceStatus });
+        var charge = _randomChargeFactory.Create(chargeId, new ChargeAttributes
+        {
+            CircumstanceId = circumstanceId,
+            Circumstance = circumstance,
+            Debtor = _randomUserFactory.Create(),
+            ChargeStatus = chargeStatus
+        });
+        circumstance.Charges = new List<Charge> { charge };
+
+        _mediatorMock
+            .Setup(mediator => mediator.Send(It.IsAny<GetChargeByIdQuery>(), CancellationToken.None))
+            .ReturnsAsync(charge);
+
+        _mediatorMock
+            .Setup(mediator => mediator.Send(It.IsAny<GetCircumstanceByIdQuery>(), CancellationToken.None))
+            .ReturnsAsync(circumstance);
+
+        _identityServiceMock
+            .Setup(service => service.CheckEqualityWithLoggedUserId(charge.DebtorId))
+            .Verifiable();
+
+        _circumstanceRepositoryMock
+            .Setup(repo => repo.UpdateAsync(It.IsAny<Circumstance>()))
+            .ReturnsAsync(true);
+
+        _chargeRepositoryMock
+            .Setup(repo => repo.UpdateAsync(It.IsAny<Charge>()))
+            .ReturnsAsync(true);
+
+        var command = new VoteForChargeCommand { ChargeId = chargeId, ChargeVote = chargeVote };
+
+        var commandHandler = new VoteForChargeCommand.VoteForChargeCommandHandler(
+            _mediatorMock.Object,
+            _chargeRepositoryMock.Object,
+            _identityServiceMock.Object,
+            _circumstanceRepositoryMock.Object
+        );
+
+        Assert.ThrowsAsync<DomainLogicException>(
+            () => commandHandler.Handle(command, CancellationToken.None));
+
+        _circumstanceRepositoryMock.Verify(repo =>
+            repo.UpdateAsync(It.IsAny<Circumstance>()), Times.Never);
+
+        _chargeRepositoryMock.Verify(repo =>
+            repo.UpdateAsync(It.IsAny<Charge>()), Times.Never);
+    }
+
+    [Fact]
+    public void VoteForCharge_InValidChargeVote_Test()
+    {
+        var chargeId = Guid.NewGuid();
+        var circumstanceId = Guid.NewGuid();
+        var circumstance = _randomCircumstanceFactory.Create(circumstanceId, new CircumstanceAttributes { CircumstanceStatus = CircumstanceStatus.New });
+        var charge = _randomChargeFactory.Create(chargeId, new ChargeAttributes
+        {
+            CircumstanceId = circumstanceId,
+            Circumstance = circumstance,
+            Debtor = _randomUserFactory.Create(),
+            ChargeStatus = ChargeStatus.New
+        });
+        circumstance.Charges = new List<Charge> { charge };
+
+        _mediatorMock
+            .Setup(mediator => mediator.Send(It.IsAny<GetChargeByIdQuery>(), CancellationToken.None))
+            .ReturnsAsync(charge);
+
+        _mediatorMock
+            .Setup(mediator => mediator.Send(It.IsAny<GetCircumstanceByIdQuery>(), CancellationToken.None))
+            .ReturnsAsync(circumstance);
+
+        _identityServiceMock
+            .Setup(service => service.CheckEqualityWithLoggedUserId(charge.DebtorId))
+            .Verifiable();
+
+        _circumstanceRepositoryMock
+            .Setup(repo => repo.UpdateAsync(It.IsAny<Circumstance>()))
+            .ReturnsAsync(true);
+
+        _chargeRepositoryMock
+            .Setup(repo => repo.UpdateAsync(It.IsAny<Charge>()))
+            .ReturnsAsync(true);
+
+        var command = new VoteForChargeCommand { ChargeId = chargeId, ChargeVote = ChargeVote.None };
+
+        var commandHandler = new VoteForChargeCommand.VoteForChargeCommandHandler(
+            _mediatorMock.Object,
+            _chargeRepositoryMock.Object,
+            _identityServiceMock.Object,
+            _circumstanceRepositoryMock.Object
+        );
+
+        Assert.ThrowsAsync<ObjectReferenceException>(
+            () => commandHandler.Handle(command, CancellationToken.None));
+
+        _circumstanceRepositoryMock.Verify(repo =>
+            repo.UpdateAsync(It.IsAny<Circumstance>()), Times.Never);
+
+        _chargeRepositoryMock.Verify(repo =>
+            repo.UpdateAsync(It.IsAny<Charge>()), Times.Never);
+    }
+}
